6b99636863f85f1e33c859d32cabd2593a50a8e0
Dom Batten <dominic.batten@googlemail.com>
2021-03-14 13:20:44 +0000

    Add movie_repository to interact with Movie model

--- infilmation/main.py
+++ infilmation/main.py
@@ -8,6 +8,7 @@ from flask import (
 
 from infilmation.database import db_session
 from infilmation.models import Movie
+from infilmation.utils.movie_repository import get_or_create_from_title
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -15,38 +16,8 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def home():
     if request.method == 'POST':
         title = request.form['title']
-        key = sha1(str.encode(title.lower().strip())).hexdigest()
-        retrieved_movie = Movie.query.filter(Movie.key == key).first()
-
-        if not retrieved_movie:
-            m = Phylm(title)
-            new_movie = Movie(
-                key=key,
-                title=m.title,
-                year=m.year,
-                genres=m.genres(),
-                runtime=m.runtime(),
-                cast=m.cast(),
-                directors=m.directors(),
-                plot=m.plot(),
-                imdb_title=m.imdb_title(),
-                imdb_year=m.imdb_year(),
-                imdb_score=m.imdb_score(),
-                imdb_low_confidence=m.imdb_low_confidence(),
-                mtc_title=m.mtc_title(),
-                mtc_year=m.mtc_year(),
-                mtc_score=m.mtc_score(),
-                mtc_low_confidence=m.mtc_low_confidence(),
-                rt_title=m.rt_title(),
-                rt_year=m.rt_year(),
-                rt_tomato_score=m.rt_tomato_score(),
-                rt_audience_score=m.rt_audience_score(),
-                rt_low_confidence=m.rt_low_confidence()
-            )
-            db_session.add(new_movie)
-            db_session.commit()
-
-        return redirect(url_for('movie.results', film=key))
+        movie = get_or_create_from_title(title)
+        return redirect(url_for('main.results', film=movie.key))
 
     return render_template('home.html')
 
---
+++ infilmation/utils/movie_repository.py
@@ -0,0 +1,56 @@
+"""Module to store some utilities based on the Movie model"""
+from hashlib import sha1
+from phylm.movie import Movie as Phylm
+from infilmation.models import Movie
+from infilmation.database import db_session
+
+
+def generate_movie_key(title):
+    """Generate a sha1 from the title to be stored as the Movie key"""
+    return sha1(str.encode(title.lower().strip())).hexdigest()
+
+
+def get_from_title(title):
+    """Return the first movie for a given title"""
+    key = generate_movie_key(title)
+    return Movie.query.filter(Movie.key == key).first()
+
+
+def store_from_title(title):
+    """Store a new movie based on a given title"""
+    key = generate_movie_key(title)
+    m = Phylm(title)
+    new_movie = Movie(
+        key=key,
+        title=m.title,
+        year=m.year,
+        genres=m.genres(),
+        runtime=m.runtime(),
+        cast=m.cast(),
+        directors=m.directors(),
+        plot=m.plot(),
+        imdb_title=m.imdb_title(),
+        imdb_year=m.imdb_year(),
+        imdb_score=m.imdb_score(),
+        imdb_low_confidence=m.imdb_low_confidence(),
+        mtc_title=m.mtc_title(),
+        mtc_year=m.mtc_year(),
+        mtc_score=m.mtc_score(),
+        mtc_low_confidence=m.mtc_low_confidence(),
+        rt_title=m.rt_title(),
+        rt_year=m.rt_year(),
+        rt_tomato_score=m.rt_tomato_score(),
+        rt_audience_score=m.rt_audience_score(),
+        rt_low_confidence=m.rt_low_confidence()
+    )
+    db_session.add(new_movie)
+    db_session.commit()
+    return new_movie
+
+
+def get_or_create_from_title(title):
+    """Return a movie if it exists or create a new one"""
+    retrieved_movie = get_from_title(title)
+    if retrieved_movie:
+        return retrieved_movie
+    return store_from_title(title)

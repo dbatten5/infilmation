77cd99076d416c7a073d385c71983ae7e33aa131
Dom Batten <dominic.batten@googlemail.com>
2021-03-16 08:54:10 +0000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header/Header';
 import './App.css';
 
 function App() {
-  const [thing, setThing] = useState(0);
-
-  useEffect(() => {
-    fetch('/thing').then(res => res.json()).then(data => {
-      setThing(data.thing);
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-         <p>The current thing is {thing}.</p>
-        </a>
-      </header>
-    </div>
+    <Header />
   );
 }
 
---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header-root">
+      <div className="Header-title-wrapper">
+        <h1 className="Header-title">Infilmation</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

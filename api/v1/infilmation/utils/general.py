1a47f40799b4d8b029939718384b140ab738af77
Dom Batten <dominic.batten@googlemail.com>
2021-03-26 09:59:54 +0000

    Reshuffle project structure
    - Separate into frontend/ and backend/

--- api/v1/infilmation/utils/general.py
+++
@@ -1,7 +0,0 @@
-"""Module for general utils"""
-
-from hashlib import sha1
-
-def generate_key(str_input):
-    """Generate a sha1 from the str_input to be stored as the Film key"""
-    return sha1(str.encode(str_input.lower().strip())).hexdigest()

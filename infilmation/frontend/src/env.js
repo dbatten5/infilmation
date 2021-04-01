30be30a8651aff1ae77053c109bc28770cf90219
Dom Batten <dominic.batten@googlemail.com>
2021-04-01 16:55:46 +0100

    Add https

--- infilmation/frontend/src/env.js
+++ infilmation/frontend/src/env.js
@@ -1,2 +1,12 @@
-export const apiUrl = `http://${process.env.REACT_APP_API_URL}`
-export const wsUrl = `ws://${process.env.REACT_APP_API_URL}`
+const env = process.env.NODE_ENV
+
+let httpProtocol = 'http';
+let wsProtocol = 'ws';
+
+if (env === 'production') {
+    httpProtocol = 'https';
+    wsProtocol = 'wss';
+}
+
+export const apiUrl = `${httpProtocol}://${process.env.REACT_APP_API_URL}`
+export const wsUrl = `${wsProtocol}://${process.env.REACT_APP_API_URL}`

819d4cd85fb48000d7375d26b058c54fe58ae817
Dom Batten <dominic.batten@googlemail.com>
2021-11-05 10:33:58 +0100

    remove old structure

--- infilmation/backend/app/app/tests/utils/test_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm.session import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/test.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
